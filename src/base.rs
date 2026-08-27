87567f72574d15795673d7fee8f8d6b239d282ab
Aapo Alasuutari <aapo.alasuutari@gmail.com>
2026-08-27 20:03:59 +0000

    fix(reborrow): recursive implementation
    * Implement Reborrow as a recursive operation
    
    If Reborrow finds '&'a mut T' fields then it inserts
    a Deref and borrow of the T, and likewise if it finds a
    'T: Reborrow' field then the field type is recursed into.
    
    This makes Reborrow always produce the correct borrow checking logic at
    the cost of most probably being inconsiderately expensive. The thinking
    is that performance will be a followup consideration.
    * PhantomDeref
    * Simpler deref test
    
    * Add more PhantomDeref unreachability assertions
    * Write out lifetime omission
    * Document ProjectionElem::PhantomDeref
    * Comment half of reborrow tests
    * fix PhantomDeref conflicting with AccessDepth::Shallow
    * Recheck CoerceShared in borrowck TypeChecker to ensure its lifetimes make sense
    * Fix rebase
    * Changes... but where to?
    * Typo fix
    
    * Improve comment
    * Use fully_perform_op to evaluate CoerceShared trait in borrowck
    * More CoerceShared comment
    * Comment rest of Reborrow tests
    
    Co-authored-by: Oli Scherer <github35764891676564198441@oli-obk.de>

--- src/base.rs
+++ src/base.rs
@@ -8,6 +8,7 @@ use rustc_ast::InlineAsmOptions;
 use rustc_codegen_ssa::base::is_call_from_compiler_builtins_to_upstream_monomorphization;
 use rustc_data_structures::profiling::SelfProfilerRef;
 use rustc_errors::DiagCtxtHandle;
+use rustc_hir::attrs::lang_items::LangItem;
 use rustc_index::IndexVec;
 use rustc_middle::ty::TypeVisitableExt;
 use rustc_middle::ty::adjustment::PointerCoercion;
@@ -390,7 +391,7 @@ fn codegen_fn_body(fx: &mut FunctionCx<'_, '_, '_>, start_block: Block) {
 
                         codegen_panic_inner(
                             fx,
-                            rustc_hir::LangItem::PanicBoundsCheck,
+                            LangItem::PanicBoundsCheck,
                             &[index, len, location],
                             *unwind,
                             source_info.span,
@@ -403,7 +404,7 @@ fn codegen_fn_body(fx: &mut FunctionCx<'_, '_, '_>, start_block: Block) {
 
                         codegen_panic_inner(
                             fx,
-                            rustc_hir::LangItem::PanicMisalignedPointerDereference,
+                            LangItem::PanicMisalignedPointerDereference,
                             &[required, found, location],
                             *unwind,
                             source_info.span,
@@ -414,7 +415,7 @@ fn codegen_fn_body(fx: &mut FunctionCx<'_, '_, '_>, start_block: Block) {
 
                         codegen_panic_inner(
                             fx,
-                            rustc_hir::LangItem::PanicNullPointerDereference,
+                            LangItem::PanicNullPointerDereference,
                             &[location],
                             *unwind,
                             source_info.span,
@@ -425,7 +426,7 @@ fn codegen_fn_body(fx: &mut FunctionCx<'_, '_, '_>, start_block: Block) {
 
                         codegen_panic_inner(
                             fx,
-                            rustc_hir::LangItem::PanicNullReferenceConstructed,
+                            LangItem::PanicNullReferenceConstructed,
                             &[location],
                             *unwind,
                             source_info.span,
@@ -437,7 +438,7 @@ fn codegen_fn_body(fx: &mut FunctionCx<'_, '_, '_>, start_block: Block) {
 
                         codegen_panic_inner(
                             fx,
-                            rustc_hir::LangItem::PanicInvalidEnumConstruction,
+                            LangItem::PanicInvalidEnumConstruction,
                             &[source, location],
                             *unwind,
                             source_info.span,
@@ -823,7 +824,11 @@ fn codegen_stmt<'tcx>(fx: &mut FunctionCx<'_, '_, 'tcx>, cur_block: Block, stmt:
                     let operand = codegen_operand(fx, operand);
                     crate::unsize::coerce_unsized_into(fx, operand, lval);
                 }
-                Rvalue::Cast(CastKind::Transmute | CastKind::Subtype, ref operand, _to_ty) => {
+                Rvalue::Cast(
+                    CastKind::Transmute | CastKind::BoxDerefTransmute | CastKind::Subtype,
+                    ref operand,
+                    _to_ty,
+                ) => {
                     let operand = codegen_operand(fx, operand);
                     lval.write_cvalue_transmute(fx, operand);
                 }
@@ -989,6 +994,7 @@ pub(crate) fn codegen_place<'tcx>(
             PlaceElem::Deref => {
                 cplace = cplace.place_deref(fx);
             }
+            PlaceElem::PhantomDeref => bug!("encountered PhantomDeref in codegen"),
             PlaceElem::OpaqueCast(ty) => bug!("encountered OpaqueCast({ty}) in codegen"),
             PlaceElem::UnwrapUnsafeBinder(ty) => {
                 cplace = cplace.place_transmute_type(fx, fx.monomorphize(ty));
@@ -1078,7 +1084,7 @@ pub(crate) fn codegen_panic_nounwind<'tcx>(
 
     codegen_panic_inner(
         fx,
-        rustc_hir::LangItem::PanicNounwind,
+        LangItem::PanicNounwind,
         &args,
         UnwindAction::Terminate(UnwindTerminateReason::Abi),
         span,
@@ -1095,7 +1101,7 @@ pub(crate) fn codegen_unwind_terminate<'tcx>(
 
 fn codegen_panic_inner<'tcx>(
     fx: &mut FunctionCx<'_, '_, 'tcx>,
-    lang_item: rustc_hir::LangItem,
+    lang_item: LangItem,
     args: &[Value],
     unwind: UnwindAction,
     span: Span,
